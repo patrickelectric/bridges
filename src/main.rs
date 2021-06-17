86cc296c7211bebdca3593f4d63d1d7ef5a63125
Patrick José Pereira <patrickelectric@gmail.com>
2021-06-17 14:53:14 -0300

    rework project
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,274 +1,124 @@
-extern crate clap;
-extern crate mio;
-extern crate mio_serial;
+#![feature(hash_drain_filter)]
 
-use clap::{App, Arg};
+use clap::{self, Clap};
 
-#[cfg(unix)]
-use mio::unix::UnixReady;
+mod links;
 
-use mio::net::UdpSocket;
-use mio::{Events, Poll, PollOpt, Ready, Token};
-use std::io::{self, Read, Write};
-use std::{str, thread, time};
+use std::io::Read;
+use std::path::PathBuf;
 
-const SERIAL_TOKEN: Token = Token(1);
-const UDP_TOKEN: Token = Token(0);
+mod socket;
 
-// ready_of_interest
-#[cfg(unix)]
-fn ready_of_interest() -> Ready {
-    Ready::readable() | UnixReady::hup() | UnixReady::error()
-}
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields
+#[derive(Clap, Debug)]
+#[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"), about = "Does serial<->UDP bridges, for now.")]
+#[clap(setting = clap::AppSettings::ColoredHelp)]
+struct Opts {
+    /// Enable verbosity
+    #[clap(short = 'v', long = "verbose")]
+    verbose: bool,
 
-#[cfg(windows)]
-fn ready_of_interest() -> Ready {
-    Ready::readable()
-}
+    /// Sets the connection serial port and baud rate, default baud rate is 115200
+    #[clap(
+        short = 'p',
+        long = "port",
+        parse(from_os_str),
+        value_name = "SERIAL_PORT:BAUD_RATE",
+        required_unless_present_any = &["available-serial-ports", "available-serial-ports-full"],
+    )]
+    serial_port: Option<PathBuf>,
 
-// is_closed
-#[cfg(unix)]
-fn is_closed(state: Ready) -> bool {
-    state.contains(UnixReady::hup() | UnixReady::error())
-}
+    /// Prints the available serial ports
+    #[clap(long = "available-serial-ports")]
+    available_serial_ports: bool,
 
-#[cfg(windows)]
-fn is_closed(_: Ready) -> bool {
-    false
+    /// Prints the available serial ports and any possible information
+    #[clap(long = "available-serial-ports-full")]
+    available_serial_ports_full: bool,
+
+    /// Sets the UDP connection ip and port
+    #[clap(
+        short = 'u',
+        long = "udp-port",
+        value_name = "IP:PORT",
+        default_value = "0.0.0.0:9092"
+    )]
+    udp_address: String,
 }
 
 pub fn main() {
-    let usb_arg_format = "SERIAL_PORT:BAUD_RATE";
-    let available_serial_ports_name = "available_serial_ports";
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .about("Does serial<->UDP bridges, for now.")
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .arg(
-            Arg::with_name("serial_port")
-                .short("p")
-                .long("port")
-                .value_name(usb_arg_format)
-                .help("Sets the connection serial port and baud rate")
-                .takes_value(true)
-                .required_unless(available_serial_ports_name),
-        )
-        .arg(
-            Arg::with_name("udp_port")
-                .short("u")
-                .long("UDP")
-                .value_name("IP:PORT")
-                .help("Sets the UDP connection ip and port")
-                .takes_value(true)
-                .default_value("0.0.0.0:9092"),
-        )
-        .arg(
-            Arg::with_name("as_client")
-                .long("as-client")
-                .help("Uses UDP connection as a client.")
-        )
-        .arg(
-            Arg::with_name("timeout")
-                .short("t")
-                .long("timeout")
-                .value_name("TIMEOUT")
-                .help("Set the timeout of UDP clients that does not communicate with server in milliseconds.")
-                .takes_value(true)
-                .default_value("10000"),
-        )
-        .arg(
-            Arg::with_name("verbose")
-                .short("v")
-                .long("verbose")
-                .help("Enables verbosity"),
-        )
-        .arg(
-            Arg::with_name(available_serial_ports_name)
-                .long("available-serial-ports")
-                .help("Prints the available serial ports."),
-        )
-        .get_matches();
+    let opts: Opts = Opts::parse();
 
-    // Check verbose mode
-    let verbose = matches.is_present("verbose");
+    let available_serial_ports = serialport::available_ports().unwrap_or_default();
 
-    if matches.is_present("available_serial_ports") {
-        println!("{:#?}", mio_serial::available_ports().unwrap());
+    if opts.available_serial_ports {
+        println!(
+            "Available serial ports: {}",
+            available_serial_ports
+                .iter()
+                .map(|port| port.port_name.clone())
+                .collect::<Vec<String>>()
+                .join(", ")
+        );
         return;
     }
 
-    // Set timeout
-    let timeout: u128 = matches
-        .value_of("timeout")
-        .unwrap()
-        .parse::<u128>()
-        .unwrap_or_else(|_| panic!("Expected u128 value got {:?}", matches.value_of("timeout")));
+    if opts.available_serial_ports_full {
+        println!("Available serial ports:\n{:#?}", available_serial_ports);
+        return;
+    }
 
-    // Configure serial port
-    let serial_arg_result = matches
-        .value_of("serial_port")
+    if !opts.serial_port.as_ref().unwrap().exists() {
+        panic!(
+            "Serial port does not exist: {}",
+            opts.serial_port.unwrap().to_str().unwrap()
+        );
+    }
+
+    let serial_arg_result = opts
+        .serial_port
+        .as_ref()
+        .unwrap()
+        .to_str()
         .unwrap()
         .split(':')
         .collect::<Vec<&str>>();
-    let (tty_path, baud_rate) = (
+
+    let (serial_path, baud_rate) = (
         serial_arg_result[0],
-        serial_arg_result[1]
+        serial_arg_result
+            .get(1)
+            .unwrap_or(&"115200")
             .parse::<u32>()
-            .unwrap_or_else(|_| panic!("Argument should follow the format {}", usb_arg_format)),
+            .unwrap_or_else(|_| panic!("Argument should follow the format {}", "IP:PORT")),
     );
 
-    let mut settings = mio_serial::SerialPortSettings::default();
-    settings.baud_rate = baud_rate;
+    let mut serial = serialport::new(serial_path, baud_rate)
+        .open()
+        .expect(&format!(
+            "Failed to open port: {} with baudrate {}",
+            serial_path, baud_rate
+        ));
 
-    println!("Opening {} with {:#?}", tty_path, settings);
-    let mut rx = mio_serial::Serial::from_path(&tty_path, &settings)
-        .expect("Something is wrong with serial path or baud rate.");
+    let socket = socket::new(&opts.udp_address)
+        .unwrap_or_else(|error| panic!("Failed to bind address: {}", error));
 
-    // TODO: Need to figure out how to wait for port to be open
-    thread::sleep(time::Duration::from_millis(100));
-
-    // Configure UDP connection
-    let addr: std::net::SocketAddr = matches
-        .value_of("udp_port")
-        .unwrap()
-        .parse()
-        .expect("Unable to parse socket address");
-
-    let socket = match matches.is_present("as_client") {
-        true => {
-            let socket = UdpSocket::bind(&"0.0.0.0:0".parse::<std::net::SocketAddr>().unwrap())
-                .expect("It was not possible to do UDP connection");
-            socket
-                .connect(addr)
-                .expect("Failed to connect, Make sure that server is running");
-            socket
-        }
-        false => UdpSocket::bind(&addr).expect("It was not possible to do UDP bind."),
-    };
-
-    println!("Server in {}", &socket.local_addr().unwrap());
-
-    // Set buffer for serial and UDP
-    let mut serial_buffer = [0u8; 4096];
-    let mut udp_buffer = [0u8; 4096];
-
-    // Hold a list of clients with timeouts
-    let mut clients = std::collections::HashMap::<std::net::SocketAddr, u128>::new();
-    // Get the time to calculate timeout of the clients
-    let now = std::time::Instant::now();
-
-    // Configure poll and events
-    let poll = Poll::new().expect("Failed to create poll.");
-    let mut events = Events::with_capacity(4096);
-
-    poll.register(&rx, SERIAL_TOKEN, ready_of_interest(), PollOpt::edge())
-        .expect("Failed to register in poll.");
-    poll.register(&socket, UDP_TOKEN, ready_of_interest(), PollOpt::edge())
-        .expect("Failed to register in poll.");
-
-    println!("Running!");
-
-    // Loop inside `is_readable` check is necessary.
-    // With edge triggered events, we must perform reading until we receive a WouldBlock.
-    // Check https://docs.rs/mio/0.6/mio/struct.Poll.html for details.
-    'outer: loop {
-        if let Err(ref e) = poll.poll(&mut events, None) {
-            println!("Poll failed: {}", e);
-            break;
-        }
-
-        // Elapsed time to calculate clients timeout
-        let elapsed_time_ms = now.elapsed().as_millis();
-
-        for event in events.iter() {
-            match event.token() {
-                UDP_TOKEN => {
-                    let ready = event.readiness();
-                    if is_closed(ready) {
-                        println!("Quitting due to event: {:?}", ready);
-                        break 'outer;
-                    }
-                    if ready.is_readable() {
-                        loop {
-                            match socket.recv_from(&mut udp_buffer) {
-                                Ok((_count, client)) => {
-                                    clients.insert(client, elapsed_time_ms);
-
-                                    if verbose {
-                                        println!("< {}: {:?}", client.ip(), &udp_buffer[.._count]);
-                                        println!(
-                                            "> {:?}: {:?}",
-                                            mio_serial::SerialPort::name(&rx).unwrap(),
-                                            &udp_buffer[.._count]
-                                        );
-                                    }
-                                    match rx.write_all(&udp_buffer[.._count]) {
-                                        Err(e) => {
-                                            println!("Error while writing in serial: {:?}", e)
-                                        }
-                                        Ok(_) => (),
-                                    };
-                                }
-                                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                                    break;
-                                }
-                                Err(ref e) => {
-                                    println!("Quitting due to read error: {}", e);
-                                    break 'outer;
-                                }
-                            }
-                        }
-                    }
-                }
-                SERIAL_TOKEN => {
-                    let ready = event.readiness();
-                    if is_closed(ready) {
-                        println!("Quitting due to event: {:?}", ready);
-                        break 'outer;
-                    }
-                    if ready.is_readable() {
-                        loop {
-                            match rx.read(&mut serial_buffer) {
-                                Ok(count) => {
-                                    if verbose {
-                                        println!(
-                                            "< {:?}: {:?}",
-                                            mio_serial::SerialPort::name(&rx).unwrap(),
-                                            &serial_buffer[..count]
-                                        );
-                                    }
-                                    clients.retain(|_, last_time| {
-                                        elapsed_time_ms - *last_time < timeout
-                                    });
-                                    for (client, _) in &clients {
-                                        if verbose {
-                                            println!(
-                                                "> {}: {:?}",
-                                                client.ip(),
-                                                &serial_buffer[..count]
-                                            );
-                                        }
-                                        match socket.send_to(&serial_buffer[..count], &client) {
-                                            Err(e) => {
-                                                println!("Error while writing in UDP: {:?}", e)
-                                            }
-                                            Ok(_) => (),
-                                        };
-                                    }
-                                }
-                                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                                    break;
-                                }
-                                Err(ref e) => {
-                                    println!("Quitting due to read error: {}", e);
-                                    break 'outer;
-                                }
-                            }
-                        }
-                    }
-                }
-                t => unreachable!("Unexpected token: {:?}", t),
+    let mut serial_vector = vec![0; 4096];
+    loop {
+        if let Ok(size) = serial.read(&mut serial_vector) {
+            let data = serial_vector[..size].to_vec();
+            if !data.is_empty() {
+                socket.write(&data);
             }
         }
+
+        let data = socket.read();
+        if !data.is_empty() {
+            serial.write(&data);
+        }
+
+        // Avoid cpu spin lock
+        std::thread::sleep(std::time::Duration::from_millis(2));
     }
 }
---
+++ src/socket.rs
@@ -0,0 +1,58 @@
+pub struct Socket {
+    socket: std::net::UdpSocket,
+    clients: std::sync::Arc<
+        std::sync::Mutex<std::collections::HashMap<std::net::SocketAddr, std::time::SystemTime>>,
+    >,
+}
+
+pub fn new(address: &str) -> Result<Socket, std::io::Error> {
+    let mut socket = std::net::UdpSocket::bind(address)?;
+    socket.set_read_timeout(Some(std::time::Duration::from_micros(100)))?;
+    Ok(Socket {
+        socket,
+        clients: std::sync::Arc::new(std::sync::Mutex::new(Default::default())),
+    })
+}
+
+impl Socket {
+    fn remove_old_clients(
+        &self,
+    ) -> std::collections::HashMap<std::net::SocketAddr, std::time::SystemTime> {
+        self.clients
+            .lock()
+            .unwrap()
+            .drain_filter(|_client, time| {
+                std::time::SystemTime::now()
+                    .duration_since(*time)
+                    .unwrap()
+                    .as_secs()
+                    > 10
+            })
+            .collect()
+    }
+
+    pub fn write(&self, data: &[u8]) {
+        self.remove_old_clients();
+        for client in self.clients.lock().unwrap().keys() {
+            if let Err(error) = self.socket.send_to(data, client) {
+                println!(
+                    "Error while writing in UDP: {:?} for client: {}",
+                    error, client
+                );
+            }
+        }
+    }
+
+    pub fn read(&self) -> Vec<u8> {
+        let mut buffer = vec![0; 4096];
+        let mut data = vec![];
+        while let Ok((size, client)) = self.socket.recv_from(&mut buffer) {
+            self.clients
+                .lock()
+                .unwrap()
+                .insert(client, std::time::SystemTime::now());
+            data.extend_from_slice(&buffer[..size]);
+        }
+        return data;
+    }
+}
