04a50d9d66d4255f1feab15b50b607a260a6d633
Patrick José Pereira <patrickelectric@gmail.com>
2021-06-17 17:00:00 -0300

    src: Add cli
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/cli.rs
@@ -0,0 +1,86 @@
+use clap::{self, Clap};
+
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields
+#[derive(Clap, Debug)]
+#[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"), about = "Does serial<->UDP bridges, for now.")]
+#[clap(setting = clap::AppSettings::ColoredHelp)]
+pub struct Options {
+    /// Enable verbosity
+    #[clap(short = 'v', long = "verbose")]
+    pub verbose: bool,
+
+    /// Enable ABR (Automatic Baud Rate) procedure
+    #[clap(long = "abr")]
+    pub automatic_baud_rate_procedure: bool,
+
+    /// Sets the connection serial port and baud rate, default baud rate is 115200
+    #[clap(
+        short = 'p',
+        long = "port",
+        value_name = "SERIAL_PORT:BAUD_RATE",
+        required_unless_present_any = &["available-serial-ports", "available-serial-ports-full"],
+    )]
+    pub serial_port: Option<String>,
+
+    /// Prints the available serial ports
+    #[clap(long = "available-serial-ports")]
+    pub available_serial_ports: bool,
+
+    /// Prints the available serial ports and any possible information
+    #[clap(long = "available-serial-ports-full")]
+    pub available_serial_ports_full: bool,
+
+    /// Sets the UDP connection ip and port
+    #[clap(
+        short = 'u',
+        long = "udp-port",
+        value_name = "IP:PORT",
+        default_value = "0.0.0.0:9092"
+    )]
+    pub udp_address: String,
+}
+
+#[derive(Debug)]
+struct Manager {
+    options: Options,
+}
+
+lazy_static! {
+    static ref MANAGER: std::sync::Arc<Manager> = std::sync::Arc::new(Manager::new());
+}
+
+impl Manager {
+    fn new() -> Self {
+        Self {
+            options: Options::parse(),
+        }
+    }
+}
+
+// Check if the verbosity parameter was used
+pub fn is_verbose() -> bool {
+    return MANAGER.as_ref().options.verbose;
+}
+
+pub fn options<'a>() -> &'a Options {
+    return &MANAGER.as_ref().options;
+}
+
+pub fn serial_port_configuration() -> (&'static str, u32) {
+    let serial_arg_result = options()
+        .serial_port
+        .as_ref()
+        .unwrap()
+        .split(':')
+        .collect::<Vec<&str>>();
+
+    (
+        serial_arg_result[0],
+        serial_arg_result
+            .get(1)
+            .unwrap_or(&"115200")
+            .parse::<u32>()
+            .unwrap_or_else(|_| panic!("Argument should follow the format {}", "IP:PORT")),
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,15 @@
 #![feature(hash_drain_filter)]
 
-use clap::{self, Clap};
+#[macro_use]
+extern crate lazy_static;
 
-use std::io::Read;
-use std::path::PathBuf;
-
+mod cli;
 mod socket;
 
-/// This doc string acts as a help message when the user runs '--help'
-/// as do all doc strings on fields
-#[derive(Clap, Debug)]
-#[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"), about = "Does serial<->UDP bridges, for now.")]
-#[clap(setting = clap::AppSettings::ColoredHelp)]
-struct Opts {
-    /// Enable verbosity
-    #[clap(short = 'v', long = "verbose")]
-    verbose: bool,
-
-    /// Enable ABR (Automatic Baud Rate) procedure
-    #[clap(long = "abr")]
-    automatic_baud_rate_procedure: bool,
-
-    /// Sets the connection serial port and baud rate, default baud rate is 115200
-    #[clap(
-        short = 'p',
-        long = "port",
-        parse(from_os_str),
-        value_name = "SERIAL_PORT:BAUD_RATE",
-        required_unless_present_any = &["available-serial-ports", "available-serial-ports-full"],
-    )]
-    serial_port: Option<PathBuf>,
-
-    /// Prints the available serial ports
-    #[clap(long = "available-serial-ports")]
-    available_serial_ports: bool,
-
-    /// Prints the available serial ports and any possible information
-    #[clap(long = "available-serial-ports-full")]
-    available_serial_ports_full: bool,
-
-    /// Sets the UDP connection ip and port
-    #[clap(
-        short = 'u',
-        long = "udp-port",
-        value_name = "IP:PORT",
-        default_value = "0.0.0.0:9092"
-    )]
-    udp_address: String,
-}
-
 pub fn main() -> Result<(), std::io::Error> {
-    let opts: Opts = Opts::parse();
-
     let available_serial_ports = serialport::available_ports().unwrap_or_default();
 
-    if opts.available_serial_ports {
+    if cli::options().available_serial_ports {
         println!(
             "Available serial ports: {}",
             available_serial_ports
@@ -66,35 +21,12 @@ pub fn main() -> Result<(), std::io::Error> {
         return Ok(());
     }
 
-    if opts.available_serial_ports_full {
+    if cli::options().available_serial_ports_full {
         println!("Available serial ports:\n{:#?}", available_serial_ports);
         return Ok(());
     }
 
-    if !opts.serial_port.as_ref().unwrap().exists() {
-        panic!(
-            "Serial port does not exist: {}",
-            opts.serial_port.unwrap().to_str().unwrap()
-        );
-    }
-
-    let serial_arg_result = opts
-        .serial_port
-        .as_ref()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .split(':')
-        .collect::<Vec<&str>>();
-
-    let (serial_path, baud_rate) = (
-        serial_arg_result[0],
-        serial_arg_result
-            .get(1)
-            .unwrap_or(&"115200")
-            .parse::<u32>()
-            .unwrap_or_else(|_| panic!("Argument should follow the format {}", "IP:PORT")),
-    );
+    let (serial_path, baud_rate) = cli::serial_port_configuration();
 
     let mut serial = serialport::new(serial_path, baud_rate)
         .open()
@@ -105,11 +37,11 @@ pub fn main() -> Result<(), std::io::Error> {
             )
         });
 
-    let socket = socket::new(&opts.udp_address)
+    let socket = socket::new(&cli::options().udp_address)
         .unwrap_or_else(|error| panic!("Failed to bind address: {}", error));
 
     // Serial and socket are ready, time to run ABR
-    if opts.automatic_baud_rate_procedure {
+    if cli::options().automatic_baud_rate_procedure {
         serial.set_break()?;
         std::thread::sleep(std::time::Duration::from_millis(10));
         serial.clear_break()?;
